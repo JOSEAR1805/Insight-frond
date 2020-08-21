19ad97a165d63d65f20078df9afcb9d00980bf6c
Alejandro Sanchez <aleverarise@gmail.com>
2020-08-21 01:52:54 -0400

    aasas

--- pages/users/index.js
+++ pages/users/index.js
@@ -120,29 +120,12 @@ const UserList = () => {
 
   const [data, setData] = useState([]);
 
-  const getCookie = (name) => {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-      const cookies = document.cookie.split(";");
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) === name + "=") {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
-    }
-    return cookieValue;
-  };
-
   const getUsers = async () => {
     // At request level
     const agent = new https.Agent({
       rejectUnauthorized: false,
     });
 
-    const csrftoken = getCookie("csrftoken");
     const payload = await axios
       .get("http://66.97.36.222/users/", { httpsAgent: agent })
       .catch((err) => console.log(err));
