3cd025d4c07b96f742c5615764c77059f9d8a951
Jose Artigas <jgam310@gmail.com>
2020-09-04 13:58:30 -0400

    icono en sidebar

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -2,15 +2,16 @@ import { Layout, Menu, Avatar } from 'antd';
 import Link from "next/link";
 import {
 	HomeOutlined,
-	DesktopOutlined,
-	TeamOutlined
+	TeamOutlined,
+	LinkOutlined,
+	AreaChartOutlined,
+	GlobalOutlined
 } from '@ant-design/icons';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
 const sidebarApp = () => {
-
 	return (
 		<Sider
 			breakpoint="sm"
@@ -25,28 +26,33 @@ const sidebarApp = () => {
 			<div className="logo" />
 			<Menu theme="dark" defaultSelectedKeys={['0']} mode="inline">
 				<Menu.Item key="0" icon={<HomeOutlined />}>
-					<Link href="/tenders/" title="Licitaciones">
+					<Link href="/" title="Inicio">
 						<a>Inicio</a>
 					</Link>
 				</Menu.Item>
-				<SubMenu key="sub1" icon={<DesktopOutlined />} title="Gestión de Búsqueda">
-					<Menu.Item key="4">
+				<Menu.Item key="1" icon={<AreaChartOutlined />}>
+					<Link href="/tenders/" title="Licitaciones">
+						<a>Licitaciones</a>
+					</Link>
+				</Menu.Item>
+				<SubMenu key="sub2" icon={<LinkOutlined />} title="Gestión de Búsqueda">
+					<Menu.Item key="21">
 						<Link href="/categories">
 							<a>Categorias</a>
 						</Link>
 					</Menu.Item>
-					<Menu.Item key="12">
+					<Menu.Item key="22">
 						<Link href="/searchSettings/">
 							<a>Configuraciones de Busquedad</a>
 						</Link>
 					</Menu.Item>
 				</SubMenu>
-				<Menu.Item key="3" icon={<DesktopOutlined />}>
+				<Menu.Item key="3" icon={<GlobalOutlined />}>
 					<Link href="/countries">
 						<a>Gestión de Paises</a>
 					</Link>
 				</Menu.Item>
-				<Menu.Item key="21" icon={<TeamOutlined />}>
+				<Menu.Item key="4" icon={<TeamOutlined />}>
 					<Link href="/users/">
 						<a>Gestión de Usuarios</a>
 					</Link>
